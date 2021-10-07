b44fcb5e896eb6971d01f5717cfc9ba155d5f31b
krollik77 <krollik77@ya.ru>
2021-10-07 14:37:38 +0300

    newWindow_DockPanel

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace OpenWorkBar
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Mini_Click(object sender, RoutedEventArgs e)
+        {
+             // это увеличивает или уменьшает экран
+                this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) //проверяет какая кнопка нажата прежде чем дать команду на перемещения окна
+                DragMove();
+
+        }
+
+        private void Maxi_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //скрывает панель TestSteps
+        private void AddNewPanelSteps_Click(object sender, RoutedEventArgs e)
+        {
+            if (TestSteps.Visibility == Visibility.Hidden)
+                TestSteps.Visibility = Visibility.Visible;
+            else
+                TestSteps.Visibility = Visibility.Hidden;
+        }
+        //скрывает панель TestPlan
+        private void AddNewPanelPlan_Click(object sender, RoutedEventArgs e)
+        {
+            if (TestPlan.Visibility == Visibility.Hidden)
+                TestPlan.Visibility = Visibility.Visible;
+            else
+                TestPlan.Visibility = Visibility.Hidden;
+        }
+
+     
     }
 }
